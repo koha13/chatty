071d3e492580fe646b5beaee2cf3bbf2d0f5ade1
khoa <policekhoa13101998@gmail.com>
2020-03-12 16:24:31 +0700

    Add persist ro user auth

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,23 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers/index";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"]
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+export const persistor = persistStore(store);
 
 export default store;
